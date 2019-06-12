bc2d14e6d302a259b9524011fce5ce549841cdae
Omarcin Espinoza <omarcin_luscius@hotmail.com>
2019-06-12 12:12:47 -0500

    se hizo la ConnectionString

--- HealthyApp/HealthyApp/Models/DbContextHealthyAppDataBase.cs
+++ HealthyApp/HealthyApp/Models/DbContextHealthyAppDataBase.cs
@@ -2,11 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace HealthyApp.Models
 {
-    public class DbContextHealthyAppDataBase
+
+        [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+
+    public class DbContextHealthyAppDataBase:DbContext
+
+
     {
+        public DbContextHealthyAppDataBase() : base("DbContextHealthyAppDataBase")
+        {
+
+
+        }
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
 
 
     }
